b125b597b90a49075cd92c9878eb2026ce19c43b
Theo-Bhang <theo.bhang@ynov.com>
2020-10-13 14:52:48 +0200

    amelioration test

--- laravel/database/seeders/UtilisateurSeeder.php
+++ laravel/database/seeders/UtilisateurSeeder.php
@@ -17,7 +17,7 @@ class UtilisateurSeeder extends Seeder
     public function run()
     {
         DB::table('utilisateurs')->insert([
-            'email' => Str::random(10).'@gmail.com',// Seed une adresse mail radom
+            'email' => Str::random(10).'@ynov.com',// Seed une adresse mail radom
             'nom' => Str::random(10),//seed un nom random
             'pseudo' => Str::random(10),//idem pour le psudo
             'mdp' => Hash::make('mdp'),//idem pour le mdp mais va le hash 
